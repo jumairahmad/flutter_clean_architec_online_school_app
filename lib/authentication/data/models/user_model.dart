702e8de97533a78b8699a297b460ec05e8db206c
Jumair Ahmad <37107267+jumairahmad@users.noreply.github.com>
2024-03-01 17:23:04 +0500

    working on models

--- lib/authentication/data/models/user_model.dart
+++ lib/authentication/data/models/user_model.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:school_course_app_1/authentication/domain/entities/user.dart';
+import 'package:school_course_app_1/core/utils/typedef.dart';
 
 class UserModel extends User {
   const UserModel(
@@ -6,6 +9,32 @@ class UserModel extends User {
       required super.name,
       required super.avatar,
       required super.createdAt});
-}
 
-class userstatus {}
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(jsonDecode(source) as DataMap);
+
+  UserModel.fromMap(DataMap map)
+      : this(
+            avatar: map['avatar'] as String,
+            id: map['id'] as int,
+            createdAt: ['createdAt'] as String,
+            name: ['name'] as String);
+
+  DataMap toMap() =>
+      {'id': id, 'avatar': avatar, 'createdAt': createdAt, 'name': name};
+
+  String toJson() => jsonEncode(toMap());
+
+  UserModel copyWith({
+    String? avatar,
+    int? id,
+    String? createdAt,
+    String? name,
+  }) {
+    return UserModel(
+        id: this.id,
+        name: this.name,
+        avatar: this.avatar,
+        createdAt: this.createdAt);
+  }
+}
--- lib/core/utils/typedef.dart
+++ lib/core/utils/typedef.dart
@@ -4,3 +4,5 @@ import 'package:school_course_app_1/core/errors/failure.dart';
 typedef ResultFuture<T> = Future<Either<Failure, T>>;
 
 typedef ResultVoid = ResultFuture<void>;
+
+typedef DataMap = Map<String, dynamic>;
