54028fc668b7439373b9513e741d21fc76e25032
Jumair Ahmad <37107267+jumairahmad@users.noreply.github.com>
2024-03-07 17:53:30 +0500

    working on domain layer repo implementation and test

--- lib/authentication/data/datasources/auth_remote_data_source.dart
+++ lib/authentication/data/datasources/auth_remote_data_source.dart
@@ -8,3 +8,20 @@ abstract class AuthRemoteDataSource {
 
   Future<List<UserModel>> getUsers();
 }
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  @override
+  Future<void> createUser(
+      {required String name,
+      required String avatar,
+      required String createdAt}) async {
+    // TODO: implement createUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<UserModel>> getUsers() async {
+    // TODO: implement getUsers
+    throw UnimplementedError();
+  }
+}
