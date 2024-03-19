cfc37bbefbf7b64e1f70b0bf05176055c33b65c0
Jumair Ahmad <37107267+jumairahmad@users.noreply.github.com>
2024-03-20 01:31:53 +0500

    working on presentation layer of authentication using block test cases

---
+++ test/authentication/presentation/cubit/authentication_cubit_test.dart
@@ -0,0 +1,59 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:school_course_app_1/authentication/domain/usecases/createUser.dart';
+import 'package:school_course_app_1/authentication/domain/usecases/get_users.dart';
+import 'package:school_course_app_1/authentication/presentation/cubit/authentication_cubit.dart';
+
+class MockGetUsers extends Mock implements GetUsers {}
+
+class MockCreateUsers extends Mock implements CreateUser {}
+
+void main() {
+  //lets write tests for the presentation of authentication
+  // for cubit only of authentication
+  late GetUsers getUsers;
+  late CreateUser createUser;
+  late AuthenticationCubit cubit;
+  const tCreateUserParam = CreateUserParams.empty();
+
+  setUp(() {
+    getUsers = MockGetUsers();
+    createUser = MockCreateUsers();
+    cubit = AuthenticationCubit(createUser: createUser, getUsers: getUsers);
+    registerFallbackValue(tCreateUserParam);
+  });
+
+  //lets tear down our cubit before testing it
+
+  tearDown(() => cubit.close());
+
+  //lets test for initial state of cubit
+
+  test('should test the cubit initial state to [AuthenticationInititals]',
+      () async {
+    expect(cubit.state, const AuthenticationInitial());
+  });
+
+  group('createuser', () {
+    blocTest('should emit [CreatingUser, UserCreated] on success',
+        build: () {
+          when(() => createUser(any()))
+              .thenAnswer((_) async => const Right(null));
+          return cubit;
+        },
+        act: (cubit) => cubit.createUser(
+            createdAt: tCreateUserParam.createdAt,
+            name: tCreateUserParam.name,
+            avatar: tCreateUserParam.avatar),
+        expect: () => const [
+              CreatingUser(),
+              UserCreated(),
+            ],
+        verify: (_) {
+          verify(() => createUser(tCreateUserParam)).called(1);
+          verifyNoMoreInteractions(createUser);
+        });
+  });
+}
