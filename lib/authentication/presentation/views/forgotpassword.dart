21173f762b6e115b19f8ea2ea89a03bcbf220626
Jumair Ahmad <37107267+jumairahmad@users.noreply.github.com>
2024-03-25 23:09:56 +0500

    initials for education app login

---
+++ lib/authentication/presentation/views/forgotpassword.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ForGotPassword extends StatelessWidget {
+  const ForGotPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
